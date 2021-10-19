9e7703bfc7ce7b0f83546199b25eb3cae4dcf0df
creatoff <creatoff@gmail.com>
2021-10-19 16:02:44 +0400

    Delete src/Components directory

--- src/Components/Wrapper/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './styles.scss';
-
-// eslint-disable-next-line react/prop-types
-function Wrapper({ children }) {
-  return (<div className="wrapper">{children}</div>);
-}
-
-export default Wrapper;
